28230ae5bd197636547928da26cbd6355704b95b
Stuart Moodie <moodie@ebi.ac.uk>
2013-05-15 11:30:40 +0100

    Added license and copyright notices

--- src/org/mbine/co/archive/ArchiveFactory.java
+++ src/org/mbine/co/archive/ArchiveFactory.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 EMBL - European Bioinformatics Institute
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of
+ * the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.mbine.co.archive;
 
 import java.io.IOException;
@@ -18,6 +33,12 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.DCTerms;
+
+/**
+ * 
+ * @author Stuart Moodie
+ *
+ */
 
 public class ArchiveFactory implements IArchiveFactory {
 	private static final String URI_PREFIX = "jar:file://";
--- src/org/mbine/co/archive/JenaTest.java
+++ src/org/mbine/co/archive/JenaTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2013 EMBL - European Bioinformatics Institute
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of
+ * the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.mbine.co.archive;
 
 import java.text.DateFormat;
@@ -11,6 +26,11 @@ import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.vocabulary.DCTerms;
 
+/**
+ * 
+ * @author Stuart Moodie
+ *
+ */
 public class JenaTest {
 	private static final String personURI = "http://somewhere/JohnSmith";
 	private static final String givenName = "John";
